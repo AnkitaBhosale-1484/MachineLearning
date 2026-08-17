8662dc42f0143c782bf29dcf39723eab65c990eb
AnkitaBhosale-1484 <bhosaleankita449@gmail.com>
2026-08-17 23:19:47 +0530

    added Assignment49

---
+++ Assignment49/Assig49q3.py
@@ -0,0 +1,31 @@
+'''Write a Python program using StandardScaler to perform feature scaling on the following dataset:
+[
+    [25, 20000],
+    [30, 40000],
+    [35, 80000]
+]
+
+Print the scaled dataset.'''
+
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+
+
+def main():
+    data=np.array([
+    [25, 20000],
+    [30, 40000],
+    [35, 80000]
+])
+
+    scaler=StandardScaler()
+    scaled_data=scaler.fit_transform(data)
+
+    print("dataset:",data)
+    print("\nscaled data:",scaled_data)
+
+
+
+
+if __name__=="__main__":
+    main()
